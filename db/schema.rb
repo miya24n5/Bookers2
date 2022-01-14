fd4a203063715ad80bc35907a50073f067d99aea
miya24n5 <sho.sakulai10@gmail.com>
2022-01-14 01:07:24 +0000

    booksのカラム変更

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_14_005728) do
+ActiveRecord::Schema.define(version: 2022_01_14_010523) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -42,10 +42,10 @@ ActiveRecord::Schema.define(version: 2022_01_14_005728) do
 
   create_table "books", force: :cascade do |t|
     t.string "title"
-    t.text "opinion"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "user_id"
+    t.text "body"
   end
 
   create_table "users", force: :cascade do |t|
